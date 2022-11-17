c14d1758ce539c29dbea35b7971a564bc3fa1748
KatsuyaKanai <kanaisilvia@yahoo.co.jp>
2022-11-17 10:52:15 +0900

    背景色変更、編集linkの追加

--- app/models/title.rb
+++ app/models/title.rb
@@ -3,7 +3,7 @@ class Title < ActiveHash::Base
 
   self.data = [
       {id: 1, name: '楽器修理、調整'}, {id: 2, name: '楽器購入'}, {id: 3, name: '消耗品購入'},
-      {id: 4, name: '楽器売却'}, {id: 5, name: 'その他'}
+      {id: 4, name: '楽器売却'}, {id: 5, name: '楽譜、アクセサリー購入'}, {id: 6, name: 'その他'}
   ]
 
 include ActiveHash::Associations
