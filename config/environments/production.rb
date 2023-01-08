2d4e6a28c6eaaf955574d0f4b36a7ef91b06d8ce
KatsuyaKanai <kanaisilvia@yahoo.co.jp>
2023-01-09 07:43:36 +0900

    gmail微修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: Rails.application.credentials.host_server[:ip] }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           => '587',
